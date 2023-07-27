50456553344a91631701c50a78d7e57fd60fbc61
Udacoding Indonesia <yusmiputrawg93@gmail.com>
2023-07-27 14:09:33 +0700

    Initial Commit

---
+++ MVVM Api/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM Api
+//
+//  Created by Yusmi Putra on 7/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
